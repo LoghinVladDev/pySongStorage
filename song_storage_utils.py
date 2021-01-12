b5c400cee662cbd761c984e9477f5d847f271285
LoghinVladDev <vlad.loghin00@gmail.com>
2021-01-12 10:38:21 +0200

    Song Delete

--- song_storage_utils.py
+++ song_storage_utils.py
@@ -1,6 +1,6 @@
 import shutil
 from db.database_manager import DatabaseManager
-import re
+import os
 
 
 def to_s(seconds_value: str):
@@ -16,6 +16,13 @@ def to_s(seconds_value: str):
 
         return int(minutes) * 60 + int(seconds)
     raise ValueError('Invalid Format of Time')
+
+
+def to_m_s(seconds: int):
+    minutes = seconds // 60
+    seconds = seconds % 60
+
+    return str(minutes) + "m " + str(seconds) + "s"
 
 
 class Command(object):
@@ -211,13 +218,7 @@ class AddSong(Command):
 
 
 class DeleteSong(Command):
-    __parameters = [
-        ('--name', 'deletes a song by title', None),
-        ('--artist', 'deletes songs by artist name', None),
-        ('--album', 'deletes songs by album', None),
-        ('--release-year', 'deletes songs by release year', None),
-        ('--tag', 'deletes songs by tag value', None, None)
-    ]
+    __parameters = []
 
     @property
     def command_text(self):
@@ -228,17 +229,33 @@ class DeleteSong(Command):
         return 'Deletes a song from the manager storage'
 
     @property
-    def param_types(self):
-        return 'Optional (instead of ID)'
+    def default_parameter(self):
+        return 'ID'
 
-    @property
-    def param_names(self):
-        param_string = ''
+    def __init__(self):
+        Command.__init__(self)
+        self.__id = 0
 
-        for p in DeleteSong.__parameters:
-            param_string += f'\t\t{p[0]} - {p[1]}\n'
+    def decode(self, string: str):
+        parts = string.split(' ', 1)
 
-        return param_string
+        if len(parts) < 1:
+            raise ValueError(f'{self.command_text} : not enough parameters')
+
+        if parts[0] != self.command_text:
+            raise ValueError(f'{self.command_text} : command name invalid')
+
+        self.__id = int(parts[1])
+
+        return self
+
+    def execute(self):
+        rows = DatabaseManager.execute_prepared('SELECT ID, file_name FROM song WHERE id = %s', (self.__id,))
+        if not rows:
+            raise ValueError(f'{self.command_text} : Not a valid ID')
+
+        DatabaseManager.execute_prepared('DELETE FROM song WHERE id=%s', (self.__id,))
+        os.remove('./storage/' + rows[0][1])
 
 
 class ModifySong(Command):
@@ -250,6 +267,91 @@ class ModifySong(Command):
         ('--duration', 'sets the duration metadata value to the value given', None),
         ('--tag', 'adds a tag for the song', None, None)
     ]
+
+    def __init__(self):
+        Command.__init__(self)
+        self.__id = 0
+
+    @property
+    def default_parameter(self):
+        return 'ID'
+
+    def decode(self, string: str):
+        parts = string.split(' ', 2)
+
+        if len(parts) < 3:
+            raise ValueError(f'{self.command_text} : not enough parameters')
+
+        if parts[0] != self.command_text:
+            raise ValueError(f'{self.command_text} : command name invalid')
+
+        self.__id = int(parts[1])
+
+        args = parts[2].split("--")
+        self.__modified_params = []
+        for arg in args:
+            if not arg:
+                continue
+
+            if '=' not in arg:
+                raise ValueError(f'{self.command_text} : format invalid. Attribute must have = between label and value')
+
+            arg_label_value = arg.split('=')
+            label = arg_label_value[0].strip()
+            value = arg_label_value[1].strip()
+
+            for p in self.__parameters:
+                if p[0].endswith(label):
+                    self.__modified_params.append((p[0], p[1], value) if p[0] != '--tag'
+                                                  else (p[0], p[1], value.split(':')[0].strip(),
+                                                        value.split(':')[1].strip()))
+
+        return self
+
+    def execute(self):
+        rows = DatabaseManager.execute_prepared('SELECT ID FROM song WHERE file_name = %s', (self.file_name,))
+
+        if rows:
+            return rows[0][0]
+
+        shutil.copy2(self.__file_path, './storage')
+        query = 'INSERT INTO song (file_name'
+        values = '(%s'
+        prep_values = [self.file_name]
+
+        for p in self.__modified_params:
+            if p[0] == '--title':
+                query += ', song_name'
+                values += ', %s'
+                prep_values.append(p[2])
+            if p[0] == '--artist':
+                query += ', artist_id'
+                values += ', %s'
+                prep_values.append(DatabaseManager.get_artist_id(p[2]))
+            if p[0] == '--album':
+                query += ', album_id'
+                values += ', %s'
+                prep_values.append(DatabaseManager.get_album_id(p[2]))
+            if p[0] == '--release-year':
+                query += ', release_year'
+                values += ', %s'
+                prep_values.append(p[2])
+            if p[0] == '--duration':
+                query += ', duration_sec'
+                values += ', %s'
+                prep_values.append(to_s(p[2]))
+
+        query += ') VALUES ' + values + ')'
+        DatabaseManager.execute_prepared(query, tuple(prep_values))
+
+        song_id = self.execute()
+
+        for p in self.__modified_params:
+            if p[0] == '--tag':
+                DatabaseManager.execute_prepared('INSERT INTO song_tag (song_id, tag_id, value) VALUES (%s, %s, %s)',
+                                                 (song_id, DatabaseManager.get_tag_id(p[2]), p[3]))
+
+        return song_id
 
     @property
     def command_text(self):
@@ -299,15 +401,13 @@ class Search(Command):
             if p[0] == '--tag':
                 tag_id = DatabaseManager.get_tag_id(p[2])
                 tag_rows = list(filter(lambda x: x[2] == p[3], DatabaseManager.execute_prepared(
-                    'SELECT song_id, tag_id, value FROM song_tag WHERE tag_id = %s', (tag_id, ))))
+                    'SELECT song_id, tag_id, value FROM song_tag WHERE tag_id = %s', (tag_id,))))
 
                 rows = list(filter(lambda x: x[0] in [song_id[0] for song_id in tag_rows], rows))
 
-
-        for row in rows:
-            print(row)
-
-        return None
+        return [{"ID": row[0], "File Name": row[1], "Title": row[2] if row[2] else "Unknown", "Release Year": row[5]
+                 if row[5] else "Unknown",
+                 "Duration": to_m_s(row[6]) if row[6] else "Unknown"} for row in rows]
 
     def __init__(self):
         Command.__init__(self)
@@ -439,5 +539,10 @@ if __name__ == '__main__':
             ' --tag = codec:flac --tag = sample rate:44100Hz').execute())
 
         print(Search().decode('Search --tag = codec:flac').execute())
+        print(Search().decode('Search ').execute())
+
+        print(DeleteSong().decode('Delete_song 3').execute())
+
+        print(Search().decode('Search ').execute())
     except ValueError as e:
         print(e)
